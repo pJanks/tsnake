42bb1b900a9780dea01f4087d488adb882cd9881
pjanks <johnny.k.cassidy@gmail.com>
2022-12-21 14:22:40 -0800

    add types to queryselected elements

--- scripts.ts
+++ scripts.ts
@@ -1,5 +1,14 @@
 // board dimensions are 600px x 350px
 // each snake piece is 10px x 10px
+const snakeGameWrapper = document.querySelector('.snake-game-wrapper') as HTMLElement;
+const snakeBoard = document.querySelector('.snake-game-canvas') as HTMLCanvasElement;
+const snakeBoardContext = snakeBoard.getContext('2d') as CanvasRenderingContext2D;
+
+// modals
+const instructionsModal = document.querySelector('.game-instructions-modal') as HTMLElement;
+const mobileNotSupportedModal = document.querySelector('.mobile-modal') as HTMLElement;
+const hiScoresModal = document.querySelector('.hi-scores-modal') as HTMLElement;
+const gameOverModal = document.querySelector('.game-over-modal') as HTMLElement;
 
 // buttons
 const closeInstructionsButton = document.querySelector('.close-instructions-button') as HTMLButtonElement;
@@ -9,80 +18,21 @@ const closeHiScoresButton = document.querySelector('.close-hi-scores-button') as
 const closeGameOverButton = document.querySelector('.close-game-over-button') as HTMLButtonElement;
 const viewHiScoresButton = document.querySelector('.view-hi-scores-button') as HTMLButtonElement;
 
-// modal sections
-const instructionsModal = document.querySelector('.game-instructions-modal') as HTMLElement;
-const mobileNotSupportedModal = document.querySelector('.mobile-modal') as HTMLElement;
-const snakeGameWrapper = document.querySelector('.snake-game-wrapper') as HTMLElement;
-const hiScoresModal = document.querySelector('.hi-scores-modal') as HTMLElement;
-const gameOverModal = document.querySelector('.game-over-modal') as HTMLElement;
-
-// target spans
+// targeted spans
 const finalScore = document.querySelector('.final-score') as HTMLElement;
 const timer = document.querySelector('.timer') as HTMLElement;
 
-// canvas elements
-const snakeBoard = document.querySelector('.snake-game-canvas') as HTMLCanvasElement;
-const snakeBoardContext = snakeBoard.getContext('2d') as CanvasRenderingContext2D;
-
-
-// check dom elements
-const throwDomError = (element: string) => {
-  const message = `${element}: undefined or null . . .`;
-  alert(message);
-  throw new Error(message);
-}
-
-if (!mobileNotSupportedModal) throwDomError('mobileNotSupportedModal'); 
-if (!closeInstructionsButton) throwDomError('closeInstructionsButton'); 
-if (!viewInstructionsButton) throwDomError('viewInstructionsButton'); 
-if (!closeHiScoresButton) throwDomError('closeHiScoresButton'); 
-if (!closeGameOverButton) throwDomError('closeGameOverButton'); 
-if (!viewHiScoresButton) throwDomError('viewHiScoresButton'); 
-if (!startOrResetButton) throwDomError('startOrResetButton'); 
-if (!snakeBoardContext) throwDomError('snakeBoardContext');
-if (!instructionsModal) throwDomError('instructionsModal'); 
-if (!snakeGameWrapper) throwDomError('snakeGameWrapper'); 
-if (!hiScoresModal) throwDomError('hiScoresModal'); 
-if (!gameOverModal) throwDomError('gameOverModal'); 
-if (!snakeBoard) throwDomError('snakeBoard'); 
-if (!finalScore) throwDomError('finalScore'); 
-if (!timer) throwDomError('timer'); 
-
-closeInstructionsButton.addEventListener('click', () => toggleModal(instructionsModal));
-viewInstructionsButton.addEventListener('click', () => toggleModal(instructionsModal));
+closeInstructionsButton.addEventListener('click', () => toggleModals(instructionsModal));
+viewInstructionsButton.addEventListener('click', () => toggleModals(instructionsModal));
 startOrResetButton.addEventListener('click', (e) => handleStartOrResetButtonClick(e));
-closeHiScoresButton.addEventListener('click', () => toggleModal(hiScoresModal));
-closeGameOverButton.addEventListener('click', () => toggleModal(gameOverModal));
-viewHiScoresButton.addEventListener('click', () => toggleModal(hiScoresModal));
-snakeBoard.addEventListener('blur', () => running || snakeBoard.focus());
+closeHiScoresButton.addEventListener('click', () => toggleModals(hiScoresModal));
+closeGameOverButton.addEventListener('click', () => toggleModals(gameOverModal));
+viewHiScoresButton.addEventListener('click', () => toggleModals(hiScoresModal));
+snakeBoard.addEventListener('blur', () => !running || snakeBoard.focus());
 snakeBoard.addEventListener('keydown', (e) => setVelocities(e));
 
-interface SnakePiece {
-  x: number,
-  y: number,
-}
-
-interface HiScores {
-  name: string,
-  score: number,
-  time: string,
-  pills_eaten: number,
-}
-
-interface TableObject {
-  intervalRunsIn: string,
-  nextPillIsWorth: number,
-  score: number,
-  pillsEaten: number,
-}
-
-interface Options {
-  method: string,
-  body: string,
-  headers: { 'content-type': string },
-}
-
-const snake: SnakePiece[] = [
+// snake always begins in the middle of the board
+const snake = [
   { x: 300, y: 180 },
   { x: 290, y: 180 },
   { x: 280, y: 180 },
@@ -90,69 +40,57 @@ const snake: SnakePiece[] = [
   { x: 260, y: 180 },
 ];
 
-let score = 0,
-  points = 100,
-  timeout = 100,
-  running = false,
-  loser = false,
-  pillsEaten = 0,
-  seconds = 0,
-  minutes = 0,
-  hours = 0,
-  xVelocity = 10,
-  yVelocity = 0,
-  keyClicked = false,
-  pillColor = '#F00',
-  hiScores: HiScores[],
-  pillXValue: number,
-  pillYValue: number,
-  interval: number;
+let pillColor = '#F00',
+running = false,
+loser = false,
+score = 0,
+timeout = 100,
+points = 100,
+keyClicked = false,
+pillsEaten = 0,
+hours = 0,
+minutes = 0,
+seconds = 0,
+xVelocity = 10,
+yVelocity = 0,
+hiScores = [],
+pillXValue,
+pillYValue,
+interval;
 
-const defaultsTable: TableObject = {
+// create and print a table to the console of defaults
+const initialTableObject = {
   intervalRunsIn: `${timeout} ms`,
   nextPillIsWorth: points,
   score,
   pillsEaten,
 };
 
-console.table(defaultsTable);
+console.table(initialTableObject);
 
-const makeNetworkRequest = async (url: string, options?: Options): Promise<void | HiScores[]> => {
-  try {
-    const response = await fetch(url, options);
-    const parsedResponse = await response.json();
-    return parsedResponse;
-  } catch (err) {
-    console.log(`there was an error: ${err}`);
-    alert(`there was an error: ${err}`);
-  }
+const makeNetworkRequest = async (url, options = {}) => {
+  const response = await fetch(url, options);
+  const parsedResponse = await response.json();
+  return parsedResponse;
 }
 
-const padNumber = (numberToPad: number): string => String(numberToPad).padStart(2, '0');
+const padNumber = number => String(number).padStart(2, '0');
 
-const toggleModal = (modal: HTMLElement): void => {
+const toggleModals = modal => {
   snakeGameWrapper.classList.toggle('hidden');
   modal.classList.toggle('hidden');
 }
 
 window.onload = () => populateHiScores();
 
-const populateHiScores = async (): Promise<void> => {
+const populateHiScores = async () => {
   try {
-    if ("ontouchstart" in document.documentElement) {
-      toggleModal(mobileNotSupportedModal);
+    if ('ontouchstart' in document.documentElement) {
+      toggleModals(mobileNotSupportedModal);
       return;
     }
 
-    const getScoresResponse = await makeNetworkRequest('/backend/get_scores.php');
-
-    if (!Array.isArray(getScoresResponse)) {
-      console.log('something is wrong with get_scores response');
-      alert('something is wrong with get_scores response');
-      return;
-    }
-
-    hiScores = getScoresResponse;
+    hiScores = await makeNetworkRequest('backend/get_scores.php');
     for (let i = 0; i < 10; i++) {
       const hiScore = hiScores[i] ?? {
         name: 'EMPTY',
@@ -160,19 +98,20 @@ const populateHiScores = async (): Promise<void> => {
         time: '00:00:00',
         pills_eaten: 0,
       };
-      const hiScoreRow = document.querySelector(`.table-data-${i}`) as HTMLElement;
-      
-      hiScoreRow.innerText = '';
+      const hiScoreRow = document.querySelector(`.table-data-${i}`);
       hiScoreRow.innerText = `${padNumber(i + 1)}. ${hiScore.name} - ${hiScore.score} - ${hiScore.time} - ${hiScore.pills_eaten} pills eaten`;
     }
+    
+    startOrResetButton.disabled = false;
+    viewInstructionsButton.disabled = false;
+    viewHiScoresButton.disabled = false;
   } catch (err) {
+    console.log(`thre was an error: ${err}`);
     viewHiScoresButton.disabled = true;
-    console.log(`there was an error: ${err}`);
-    alert(`there was an error: ${err}`);
   }
 }
 
-const drawSnake = (): void => {
+const drawSnake = () => {
   snake.forEach((part, i) => {
     !i ? snakeBoardContext.fillStyle = '#FF0' : snakeBoardContext.fillStyle = '#28BD00';
     snakeBoardContext.fillRect(part.x, part.y, 10, 10);
@@ -180,21 +119,27 @@ const drawSnake = (): void => {
   });
 }
 
-const populatePill = async (x?: number, y?: number): Promise<void> => {
-
+const populatePill = async (x = null, y = null) => {
+  let pillIsOnOrAroundSnake = false;
   if (!x || !y) {
+
     // get random 10x10 blocks on the canvas for pill placement
     // add five to center the pill in the square on the grid
     const possibleX = Math.random() * 60 + 5;
     const possibleY = Math.random() * 35 + 5;
     
-    // make sure the random coordinates are not on borders or on top of the snake
+    // make sure the random coordinates are not on top of the snake
     snake.forEach(part => {
-      if (possibleX * 10 - part.x <= 5 && possibleX * 10 - part.x >= -5 && possibleY * 10 - part.y <= 5 && possibleY * 10 - part.y >= -5 || possibleX * 10 < 5 || possibleX * 10 > 595 || possibleY * 10 < 5 || possibleY * 10 > 345) {
-        populatePill();
-        return;
+      if (possibleX * 10 - part.x <= 5 && possibleX * 10 - part.x >= -5 && possibleY * 10 - part.y <= 5 && possibleY * 10 - part.y >= -5) {
+        pillIsOnOrAroundSnake = true;
       }
     });
+
+    // make sure random coordinates are not off the border
+    if (possibleX * 10 < 5 || possibleX * 10 > 595 || possibleY * 10 < 5 || possibleY * 10 > 345 || pillIsOnOrAroundSnake) {
+      populatePill();
+      return;
+    }
 
     // scale values to actual columns and rows
     pillXValue = Math.round(possibleX) * 10 + 5;
@@ -210,47 +155,54 @@ const populatePill = async (x?: number, y?: number): Promise<void> => {
   snakeBoardContext.closePath();
 }
 
-const handleStartOrResetButtonClick = (e: Event): void => {
-  const target = e.target as HTMLButtonElement;
-  if (target.innerText.toLowerCase() !== 'reset') {
-    target.innerText = 'Reset';
+const handleStartOrResetButtonClick = (e) => {
+  if (e.target.innerText.toLowerCase() !== 'reset') {
+    e.target.innerText = 'Reset';
+
     viewInstructionsButton.disabled = true;
     viewHiScoresButton.disabled = true;
+  
     interval = setInterval(adjustTimes, 1000);
+  
     snakeBoard.focus();
+  
     runGame();
     running = true;
-  } else location.reload();
+  } else {
+    location.reload();
+  }
 }
 
-const adjustTimes = (): void => {
+const adjustTimes = () => {
   seconds++;
+
   if (seconds === 60) {
     minutes++;
     seconds = 0;
     points += 3;
-    console.log('three extra points added for a minute');
+    console.log('extra points added for a minute');
   }
+
   if (minutes === 60) {
     hours++;
     minutes = 0;
     points += 13;
-    console.log('thirteen extra points added for an hour');
+    console.log('extra points added for an hour');
   }
 
   timer.innerText = `${padNumber(hours)}:${padNumber(minutes)}:${padNumber(seconds)}`;
 }
 
-const runGame = async (): Promise<void> => {
+const runGame = async () => {
   if (loser) {
-    
-    toggleModal(gameOverModal);
-    finalScore.innerText = String(score);
-    closeGameOverButton.focus();
     viewInstructionsButton.disabled = false;
     viewHiScoresButton.disabled = false;
 
-    if (hiScores && !hiScores[9] || score > Number(hiScores[9].score)) {
+    toggleModals(gameOverModal);
+    closeGameOverButton.focus();
+    finalScore.innerText = score;
+
+    if (!hiScores[9] || score > Number(hiScores[9].score)) {
       const name = prompt(`
         Congrats, You\'ve scored in the top 10!!
         Please enter an identifier:
@@ -272,21 +224,21 @@ const runGame = async (): Promise<void> => {
   }
 }
 
-const clearCanvas = (): void => {
+const clearCanvas = () => {
   snakeBoardContext.fillStyle = '#000';
   snakeBoardContext.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
   snakeBoardContext.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
 }
 
-const moveSnake = (): void => {  
-
-  // where the snakes head will be next
-  const head: SnakePiece = {
+const moveSnake = () => { 
+  
+  // where the snake will be next
+  const head = {
     x: snake[0].x + xVelocity,
     y: snake[0].y + yVelocity,
   };
 
-  // check for collisions
+  // check for collision with walls
   if (head.x === -10 || head.x === 600 || head.y === -10 || head.y === 350 || checkForTailCollision(head)) {
     loser = true;
     running = false;
@@ -300,7 +252,7 @@ const moveSnake = (): void => {
   }
 }
 
-const checkForTailCollision = (head: SnakePiece): boolean => {
+const checkForTailCollision = (head) => {
   let collidedWithTail = false;
   snake.forEach(part => {
     if (head.x === part.x && head.y === part.y) {
@@ -310,7 +262,7 @@ const checkForTailCollision = (head: SnakePiece): boolean => {
   return collidedWithTail;
 }
 
-const checkForPillCollision = (head: SnakePiece): boolean => {
+const checkForPillCollision = head => {
   if ((xVelocity && head.x + 5 === pillXValue) && head.y + 5 === pillYValue || (yVelocity && head.y + 5 === pillYValue) && head.x + 5 === pillXValue) {
 
     pillColor === '#F00' ? pillColor = '#00F' : pillColor = '#F00';
@@ -322,59 +274,48 @@ const checkForPillCollision = (head: SnakePiece): boolean => {
     pillsEaten++;
     timeout = Number((timeout - .04).toFixed(2));
 
-    const scoreDetails: TableObject = {
+    const tableObject = {
       intervalRunsIn: `${timeout} ms`,
       nextPillIsWorth: points,
       score,
       pillsEaten,
     };
-    console.table(scoreDetails);
+    console.table(tableObject);
     return true;
   }
-  return false;
 }
 
 // update velocities based on keypresses
 // and make sure that you can't move backwards into your self
-const setVelocities = (e: KeyboardEvent): void => {
+const setVelocities = (e) => {
   if (!keyClicked) {
     keyClicked = true;
-    const key = e.key.toLowerCase();
-    if (!xVelocity && (key === 'a' || key === 'arrowleft')) {
+    if (!xVelocity && e.key.toLowerCase() === 'a') {
       xVelocity = -10;
       yVelocity = 0;
-    } else if (!xVelocity && (key === 'd' || key === 'arrowright')) {
+    } else if (!xVelocity && e.key.toLowerCase() === 'd') {
       xVelocity = 10;
       yVelocity = 0;
-    } else if (!yVelocity && (key === 'w' || key === 'arrowup')) {
+    } else if (!yVelocity && e.key.toLowerCase() === 'w') {
       xVelocity = 0;
       yVelocity = -10;
-    } else if (!yVelocity && (key === 's' || key === 'arrowdown')) {
+    } else if (!yVelocity && e.key.toLowerCase() === 's') {
       xVelocity = 0;
       yVelocity = 10;
     }
   }
 }
 
-const insertScore = async (name: string): Promise<void> => {
-  if (viewHiScoresButton.disabled) {
-    console.log('db doesn\'t seem to be up');
-    return;
-  }
-  try {
-    const time = timer.innerText;
-    const options: Options = {
-      method: 'POST',
-      body: JSON.stringify({ score, name, time, pillsEaten }),
-      headers: {
-        'content-type': 'application/json',
-      },
-    };
-    await makeNetworkRequest('/backend/insert_scores.php', options);
-  } catch (err) {
-    console.log(`there was an error: ${err}`);
-    alert(`there was an error: ${err}`);
-  }
+const insertScore = async (name) => {
+  const time = timer.innerText;
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({ score, name, time, pillsEaten }),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  await makeNetworkRequest('backend/insert_scores.php', options);
 }
 
 drawSnake();
